58c1cd2c0708065d5016fbf7b6bd5e116f120e70
RahulRST <srahuljayaraj@gmail.com>
2023-10-26 15:37:33 +0530

    Ex-5 in Progress

--- Ex-5/WEB-INF/classes/SpaceSession.java
+++ Ex-5/WEB-INF/classes/SpaceSession.java
@@ -2,15 +2,14 @@ import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 
-class SpaceSession extends HttpSession {
-    @override
+class SpaceSession extends HttpServlet {
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        HttpSession session = new HttpSession();
+        HttpSession session = request.getSession();
         session.setAttribute("planet",request.getParameter("planet"));
         PrintWriter out = response.getWriter();
         out.println("<html><body>");
         out.println("<h1>Session Created</h1>");
-        out.println("<a href='RestoreSpaceSession'>Click here to see the session</a>");
+        out.println("<a href='sestorespacesession'>Click here to see the session</a>");
         out.println("</body></html>");
     }
 }
