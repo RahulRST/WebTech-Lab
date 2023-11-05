1b5a654981d0d7d404e57e351647517816342f9e
RahulRST <srahuljayaraj@gmail.com>
2023-11-05 15:21:02 +0530

    Servlet Sessions Done

--- Ex-5B/WEB-INF/classes/RestoreSpaceSession.java
+++ Ex-5B/WEB-INF/classes/RestoreSpaceSession.java
@@ -4,10 +4,11 @@ import javax.servlet.http.*;
 
 public class RestoreSpaceSession extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        HttpSession session = request.getSession();
+        HttpSession session = request.getSession(false);
         PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>Session Restored</h1>");
+        out.println("<html><head>");
+        out.println("<link rel='stylesheet' href='style.css' /> <title>Restore Space Session</title></head>");
+        out.println("<body class='flex-col'><h1>Session Restored</h1>");
         out.println("<h2>Planet: " + session.getAttribute("planet") + "</h2>");
         out.println("</body></html>");
     }
--- Ex-5B/WEB-INF/classes/SpaceSession.java
+++ Ex-5B/WEB-INF/classes/SpaceSession.java
@@ -2,14 +2,15 @@ import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 
-class SpaceSession extends HttpServlet {
+public class SpaceSession extends HttpServlet {
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         HttpSession session = request.getSession();
         session.setAttribute("planet",request.getParameter("planet"));
         PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>Session Created</h1>");
-        out.println("<a href='sestorespacesession'>Click here to see the session</a>");
+        out.println("<html><head>");
+        out.println("<link rel='stylesheet' href='style.css' /> <title>Space Session</title></head>");
+        out.println("<body class='flex-col'><h1>Session Created</h1>");
+        out.println("<a href='restorespacesession'>Click here to see the session</a>");
         out.println("</body></html>");
     }
 }
